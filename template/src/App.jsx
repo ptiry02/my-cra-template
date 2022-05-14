c4e8a754dc9a4a8c7bfeb3ea2ee7f195fec25217
Paul Tiry <tirypaul@gmail.com>
2022-05-14 12:14:23 +0200

    bug fixes

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Title>Happy Coding!</Title>
 }
 
-export default App;
+export default App
+
+const Title = styled.h1`
+  width: 100vw;
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
